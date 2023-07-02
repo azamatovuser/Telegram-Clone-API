8d118805f521d78322bf28dbcaeaa62f9a522abd
Azamat <azamatovuser@gmail.com>
2023-07-02 06:33:49 +0500

    changes

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import RoomListCreateAPIView, RoomWithMessageAPIView
+from .views import RoomListCreateAPIView, RoomWithMessageAPIView, MessageCreateAPIView
 
 
 urlpatterns = [
     path('rooms/', RoomListCreateAPIView.as_view()),
     path('rooms/<int:pk>/', RoomWithMessageAPIView.as_view()),
+    path('rooms/message/create/', MessageCreateAPIView.as_view()),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from .models import Room, Message
 from .serializers import RoomSerializer, MessageSerializer, RoomWithMessagesSerializer
 
@@ -8,8 +10,34 @@ class RoomListCreateAPIView(generics.ListCreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+    # def get_queryset(self):
+    #     qs = super().get_queryset()
+    #     search = self.kwargs.get('search')
+    #     if qs:
+    #         qs = qs.filter(account__full_name__icontains=search)
+    #     return qs
 
-class RoomWithMessageAPIView(generics.RetrieveAPIView ):
+
+class RoomWithMessageAPIView(generics.RetrieveAPIView):
     # http://127.0.0.1:8000/chat/rooms/room_id/
     queryset = Room.objects.all()
     serializer_class = RoomWithMessagesSerializer
+
+
+class MessageCreateAPIView(APIView):
+    # http://127.0.0.1:8000/chat/rooms/message/create/
+    def post(self, request):
+        account = request.data.get('account')
+        room = request.data.get('room')
+        message = request.data.get('message')
+        if message and room and account:
+            Message.objects.create(account_id=account, room_id=room, message=message)
+            return Response({"detail": "created"})
+        return Response({"detail": "invalid data"})
+
+    # Example of sending data
+    # {
+    #   "account":1,
+    #   "room":5,
+    #   "message":"test"
+    # }
